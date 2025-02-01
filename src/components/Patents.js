ce3d38b15c04f8dde6c757eccd3de90d302770c8
Mausam Jain <mausamsion@gmail.com>
2025-02-01 23:23:35 +0530

    add new

--- src/components/Patents.js
+++ src/components/Patents.js
@@ -11,6 +11,9 @@ function Patents() {
                     <li>
                         <a href='https://patentscope2.wipo.int/search/en/detail.jsf?docId=WO2018180239'>Information Processing Device and Method</a> - prediction of internal greenhouse environment and minimization of sensor installation and maintenance cost using machine learning.
                     </li>
+                    <li>
+                        <a href='https://www.j-platpat.inpit.go.jp/c1801/PU/JP-7588629/15/en'>Learning System and Method for Text Similarity</a> - novel technique to perform the FAQ retrieval given user query as input.
+                    </li>
                 </ol>
             </Card.Body>
         </Card>
